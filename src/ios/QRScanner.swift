83b1ceda4dde4c9ff281324451bc20bac78d4255
Bart Blokhuis <72574191+bartblokhuis@users.noreply.github.com>
2023-02-06 11:28:51 +0100

    fix: Swift compile fix

--- src/ios/QRScanner.swift
+++ src/ios/QRScanner.swift
@@ -245,6 +245,8 @@ class QRScanner : CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
             // while nothing is detected, or if scanning is false, do nothing.
             return
         }
+        
+        
         let found = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
         if ((found.type == AVMetadataObject.ObjectType.qr || 
                 found.type == AVMetadataObject.ObjectType.code39Mod43 || 
@@ -484,7 +486,7 @@ class QRScanner : CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
 
     @objc func openSettings(_ command: CDVInvokedUrlCommand) {
         if #available(iOS 10.0, *) {
-            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
             return
         }
         if UIApplication.shared.canOpenURL(settingsUrl) {
@@ -497,7 +499,7 @@ class QRScanner : CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
         } else {
             // pre iOS 10.0
             if #available(iOS 8.0, *) {
-                UIApplication.shared.openURL(NSURL(string: UIApplication.openSettingsURLString)! as URL)
+                UIApplication.shared.openURL(NSURL(string: UIApplicationOpenSettingsURLString)! as URL)
                 self.getStatus(command)
             } else {
                 self.sendErrorCode(command: command, error: QRScannerError.open_settings_unavailable)
